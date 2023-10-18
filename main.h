26a6190543b77edd2f30c60da0e2c8ff692ac179
SHEBLYSTA <140198363+Sheblysta@users.noreply.github.com>
2023-10-18 03:08:02 +0100

    Update main.h

--- main.h
+++ main.h
@@ -45,7 +45,7 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
-/* Funtions to print chars and strings */
+/* these Funtions prints chars and strings */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
